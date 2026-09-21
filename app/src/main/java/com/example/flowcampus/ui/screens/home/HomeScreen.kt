006ee1a30cdf221c6d61c6acdff058d990c39055
anna0lbi05 <anna.co@puccampinas.edu.br>
2026-09-20 22:29:56 -0300

    test: add initial home screen

---
+++ app/src/main/java/com/example/flowcampus/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.flowcampus.ui.screens.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "FlowCampus")
+}
